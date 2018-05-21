57246ab2c6425c93e16e24e0d125f9371b286c6c
Nutti Saelor <leelorz6@gmail.com>
2018-05-22 01:22:22 +0700

    add expanding item with animation

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation,
+  NativeModules
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
+const { UIManager } = NativeModules;
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription() {
+    const { item, expanded } = this.props;
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={{ flex: 1 }}>{item.description}</Text>
+        </CardSection>
+      );
+    }
+  }
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.item;
+
     return (
       <TouchableWithoutFeedback onPress={() => this.props.selectLibrary(id)}>
         <View>
           <CardSection>
             <Text style={titleStyle}>{title}</Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -27,4 +53,10 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.item.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
